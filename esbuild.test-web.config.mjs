91c6a03053c7031b70e87115c80ab0d7c32f0740
hankei6km <gh-hankei6km@outlook.jp>
2023-07-16 12:49:49 +0000

    chore: Refactor `esbuild.test-web.config.mjs`
    - unused plugin is removed
    - commented lines is removed

--- esbuild.test-web.config.mjs
+++ esbuild.test-web.config.mjs
@@ -1,5 +1,4 @@
 import * as esbuild from 'esbuild'
-import * as globalsPlugin from '@esbuild-plugins/node-globals-polyfill'
 import * as modulesPlugin from '@esbuild-plugins/node-modules-polyfill'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
@@ -21,7 +20,6 @@ const files = fs
         type: 'ImportExpression',
         source: {
           type: 'Literal',
-          // value: `./${f.replace(/\.ts$/, '.js')}`
           value: `../suite/${f.replace(/\.ts$/, '.js')}`
         }
       }
@@ -30,7 +28,6 @@ const files = fs
 const p = {
   type: 'Program',
   sourceType: 'script',
-  //comments: []
   body: [
     {
       type: 'FunctionDeclaration',
@@ -45,45 +42,14 @@ const p = {
       body: {
         type: 'BlockStatement',
         body: files
-        //body: [
-        //  {
-        //    type: 'ExpressionStatement',
-        //    expression: {
-        //      type: 'AwaitExpression',
-        //      argument: {
-        //        type: 'ImportExpression',
-        //        source: {
-        //          type: 'Literal',
-        //          value: 'test.js'
-        //        }
-        //      }
-        //    }
-        //  },
-        //  {
-        //    type: 'ExpressionStatement',
-        //    expression: {
-        //      type: 'AwaitExpression',
-        //      argument: {
-        //        type: 'ImportExpression',
-        //        source: {
-        //          type: 'Literal',
-        //          value: 'test1.js'
-        //        }
-        //      }
-        //    }
-        //  }
-        //]
       }
     }
   ]
 }
 const importAllFunc = toJs(p).value
-//const importAllFunc = ''
 
 await esbuild.build({
-  //entryPoints: ['src/test/index.ts'],
   stdin: {
-    //contents: c.outputFiles[0].text,
     contents: `${fs.readFileSync(
       'src/test/web/index.ts',
       'utf8'
@@ -91,20 +57,9 @@ await esbuild.build({
     loader: 'ts',
     resolveDir: 'src/test/web'
   },
-  //inject: ['node_modules/mocha/mocha.js'],
   outfile: 'out/test/web/index.js',
   bundle: true,
-  plugins: [
-    globalsPlugin.NodeGlobalsPolyfillPlugin({
-      process: true,
-      define: {
-        //'process.env.NODE_ENV': deploy ? '"production"' : '"development"'
-        'process.env.NODE_ENV': '"development"'
-      }
-    }),
-    modulesPlugin.NodeModulesPolyfillPlugin()
-  ],
-  //define: { suiteFiles: '["./extension.test.js"]' },
+  plugins: [modulesPlugin.NodeModulesPolyfillPlugin()],
   external: ['vscode'],
   sourcemap: true,
   platform: 'browser',
